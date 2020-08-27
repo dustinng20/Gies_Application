e447531cd8cb4897e4e906a06ba3a11a9757fb83
her_usernameher_username <58968686+dunghk123@users.noreply.github.com>
2020-08-27 12:16:41 +0700

    Add project files.

---
+++ Gies_Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gies_Application.MvcApplication" Language="C#" %>
---
+++ Gies_Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gies_Application.Startup))]
+namespace Gies_Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
