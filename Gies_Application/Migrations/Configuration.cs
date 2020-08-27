abc43c060526b130b58a79b62c940425bf9452eb
her_usernameher_username <58968686+dunghk123@users.noreply.github.com>
2020-08-27 12:31:23 +0700

    Add model

---
+++ Gies_Application/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Gies_Application.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Gies_Application.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Gies_Application.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Gies_Application/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gies_Application.Models
+{
+	public class Course
+	{
+		public Course()
+		{
+			this.TraineeInfoes = new HashSet<TraineeInfo>();
+		}
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public ICollection<Topic> Topics { get; set; }
+		[Display(Name = "Trainees")]
+		public ICollection<TraineeInfo> TraineeInfoes { get; set; }
+
+		public CourseCategory CourseCategory { get; set; }
+	}
+}
