3fe9271acf12ff1a0d5e6bc8fef6e4e44a1e3fb9
her_usernameher_username <58968686+dunghk123@users.noreply.github.com>
2020-08-30 03:08:05 +0700

    role inbsign in

---
+++ Gies_Application/Migrations/202008292005415_admin2.cs
@@ -0,0 +1,16 @@
+﻿namespace Gies_Application.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class admin2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
